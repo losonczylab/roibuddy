ede2fdd3b89f5bf29b9741c47af4334ae9516ce6
Patrick Kaifosh <pwk2108@columbia.edu>
2015-01-28 18:09:02 -0500

    Fixes to get setup.py working

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup
 setup(
     name='ROIBuddy',
-    version='0.2.1',
+    version='1.0.0-dev',
     author_email='software@losonczylab.org',
     url='http://www.losonczylab.org/sima/roibuddy',
     license='GNU General Public License (GPL)',
     packages=['ROIBuddy'],
-    package_dir={'ROIBuddy': '.'},
     entry_points={'gui_scripts': ['roibuddy = ROIBuddy.roi_buddy:main']}
 )
