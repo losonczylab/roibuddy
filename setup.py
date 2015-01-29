8a1b2b7078834691e005c2a91ef71bc0d1532ebf
Patrick Kaifosh <pwk2108@columbia.edu>
2015-01-29 14:07:15 -0500

    Adjusted setup script and renamed package.

--- setup.py
+++ setup.py
@@ -1,10 +1,34 @@
 from setuptools import setup
+
+CLASSIFIERS = """\
+Development Status :: 4 - Beta
+Intended Audience :: Science/Research
+License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)
+Operating System :: MacOS
+Operating System :: Microsoft :: Windows
+Operating System :: POSIX
+Operating System :: Unix
+Programming Language :: Python
+Topic :: Scientific/Engineering
+
+"""
+
 setup(
-    name='ROIBuddy',
+    name='roibuddy',
     version='1.0.0-dev',
-    author_email='software@losonczylab.org',
-    url='http://www.losonczylab.org/sima/roibuddy',
-    license='GNU General Public License (GPL)',
-    packages=['ROIBuddy'],
-    entry_points={'gui_scripts': ['roibuddy = ROIBuddy.roi_buddy:main']}
+    packages=['roibuddy'],
+    entry_points={'gui_scripts': ['roibuddy = roibuddy.roi_buddy:main']},
+    install_requires=[
+        'sima',
+        'guiqwt>=2.1.6',
+        'guidata>=1.4.1',
+    ],
+    author="The SIMA Development Team",
+    author_email="software@losonczylab.org",
+    description="GUI for editing segmentations when using SIMA",
+    license="GNU GPLv2",
+    url="http://www.losonczylab.org/sima/",
+    keywords="imaging microscopy neuroscience segmentation",
+    classifiers=[_f for _f in CLASSIFIERS.split('\n') if _f],
+    platforms=["Linux", "Mac OS-X", "Windows"],
 )
